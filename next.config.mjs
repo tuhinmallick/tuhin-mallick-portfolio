d845ff9f18cedfefa2e9d5e2c406e7245d7f27f7
Tuhin Mallick <tuhin.mllk@gmail.com>
2024-07-23 01:02:32 +0200

    chore: Add rewrite rule for homepage animation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/',
+          destination: '/home-dark-animation',
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
